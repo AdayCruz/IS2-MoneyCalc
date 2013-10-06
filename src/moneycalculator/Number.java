41d42d7616d106c67913c468a0043dc4793e388b
Aday <Aday@PCA>
2013-10-06 19:36:36 +0100

    Second commit

--- src/moneycalculator/Number.java
+++ src/moneycalculator/Number.java
@@ -1,5 +1,26 @@
 package moneycalculator;
 
 public class Number {
+    private final long numerator;
+    private final long denominator;
+
+    public Number(long numerator, long denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
     
+    public Number(long numerator){
+        this(numerator, 1);
+    }
+    
+    public Number add(Number number){
+        long numerator;
+        long denominator;
+        
+        denominator=(this.denominator)*(number.denominator);
+        numerator=((denominator/this.denominator)*this.numerator)+
+                ((denominator/number.denominator)*number.numerator);
+        
+        return this(numerator,denominator);
+    }
 }
