a665f63ff3ad902f41ff9a5229444771a96e7e02
Aday <Aday@PCA>
2013-10-10 18:55:10 +0100

    Fifth commith

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -3,6 +3,8 @@ package moneycalculator;
 public class MoneyCalculator {
 
     public static void main(String[] args) {
-        
+        Number myNumber = new Number(1.95);
+        System.out.println(myNumber.getNumerator());
+        System.out.println(myNumber.getDenominator());
     }
 }
